ab5aa73ee397edaefeb540b9af2ac6685f76d770
ansakhar <anna.a.sakharovskaya@student.jyu.fi>
2021-11-25 10:05:21 +0200

    image01

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
   console.log("Database connected")
-  console.log("test")
 })
 
 // initial page: login-register form
